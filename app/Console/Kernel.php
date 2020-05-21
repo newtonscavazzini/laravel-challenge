476095617dba1b59d9cc86d377d7b90bdbc1bbdd
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-05-21 05:12:13 -0300

    Scheduling upcoming events job to daily at midnight

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\NotifyUpcomingEvents;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -26,6 +27,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->job(new NotifyUpcomingEvents)
+            ->dailyAt('00:00');
     }
 
     /**
